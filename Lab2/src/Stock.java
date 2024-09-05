67d63e15c061c98de343fa09a8f96adccd16a4ef
Alexandre Labranche <alexandrelabranche18@gmail.com>
2024-09-05 15:41:18 -0400

    Added new project file

---
+++ Lab2/src/Stock.java
@@ -0,0 +1,14 @@
+public class Stock {
+    public static void main(String[] args) {
+
+        final int NUM_SHARES = 1000;            // # of shares
+        final double PURCHASE_PRICE = 32.87;    // Purchase price per share
+        final double SELLING_PRICE = 33.92;     // Selling price per share
+        final double BROKER_COM_RATE = 0.02;    // Broker commission rate
+
+        // Cost of stock without commission
+        double stockPurchase = NUM_SHARES * PURCHASE_PRICE;
+
+        // Brokers commission on the purchase
+    }
+}
