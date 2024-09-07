6990f04e08d8a8581381376f4cdbfe760c5271e2
Alexandre Labranche <alexandrelabranche18@gmail.com>
2024-09-06 22:05:37 -0400

    Finished Star assignment

--- Lab2/src/Star.java
+++ Lab2/src/Star.java
@@ -1,8 +1,12 @@
 public class Star {
     public static void main(String[] args) {
 
-        String stars="*******";
-
-    System.out.printf("%10s\n", stars);
+        String stars = "*******";
+        System.out.print("  *\n");
+        System.out.print(" *** \n");
+        System.out.print(" *****\n");
+        System.out.printf("%7s\n", stars);
+        System.out.print(" ***\n");
+        System.out.print("  *");
     }
 }
