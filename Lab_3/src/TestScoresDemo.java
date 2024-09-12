91e75d2fc3a8498fd2daa0e85380acb6db6e6d07
Alexandre Labranche <alexandrelabranche18@gmail.com>
2024-09-12 19:34:43 -0400

    Renamed scanner variable

--- Lab_3/src/TestScoresDemo.java
+++ Lab_3/src/TestScoresDemo.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 public class TestScoresDemo {
 
     public static void main(String[] args) {
-        Scanner t1 = new Scanner(System.in);
+        Scanner scn = new Scanner(System.in);
 
         TestScores score = new TestScores();
 
         System.out.println("Enter score #1 ");
-        score.setTest1(t1.nextDouble());
+        score.setTest1(scn.nextDouble());
 
         System.out.println("Enter score #2 ");
-        score.setTest2(t1.nextDouble());
+        score.setTest2(scn.nextDouble());
 
 
         System.out.println("Enter score #3 ");
-        score.setTest3(t1.nextDouble());
+        score.setTest3(scn.nextDouble());
 
         TestScores average = new TestScores();
 
