7a084099071180f4ac32518478b938f8e35f5933
Alexandre Labranche <alexandrelabranche18@gmail.com>
2024-09-12 15:15:41 -0400

    Started some new projects in Lab 3

--- Lab_3/src/TestScoresDemo.java
+++ Lab_3/src/TestScoresDemo.java
@@ -2,25 +2,23 @@ import java.util.Scanner;
 public class TestScoresDemo {
 
     public static void main(String[] args) {
-
-        TestScores test1 = new TestScores();
         Scanner t1 = new Scanner(System.in);
+
+        TestScores score = new TestScores();
+
         System.out.println("Enter score #1 ");
-        test1.setTest1(t1.nextDouble());
+        score.setTest1(t1.nextDouble());
 
-        TestScores test2 = new TestScores();
-        Scanner t2 = new Scanner(System.in);
         System.out.println("Enter score #2 ");
-        test2.setTest2(t2.nextDouble());
+        score.setTest2(t1.nextDouble());
 
-        TestScores test3 = new TestScores();
-        Scanner t3 = new Scanner(System.in);
+
         System.out.println("Enter score #3 ");
-        test3.setTest3(t3.nextDouble());
+        score.setTest3(t1.nextDouble());
 
         TestScores average = new TestScores();
 
-        System.out.println("The average of these three scores is " + average.getAverage() );
+        System.out.println("The average of these three scores is " + score.getAverage() );
 
     }
 }
