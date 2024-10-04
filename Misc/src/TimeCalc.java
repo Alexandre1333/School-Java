d083f3fe5531a658f4a08343a1dda0d8daa71fcf
Alexandre Labranche <alexandrelabranche18@gmail.com>
2024-10-04 13:02:56 -0400

    added timecalc as misc project

---
+++ Misc/src/TimeCalc.java
@@ -0,0 +1,13 @@
+public class TimeCalc {
+    private double seconds;
+    private double minutes;
+    private double hours;
+    private double days;
+
+    public TimeCalc(double seconds, double minutes, double hours, double days) {
+        this.seconds = seconds;
+        this.minutes = minutes;
+        this.hours = hours;
+        this.days = days;
+    }
+}
