99b3b31d7e77784243ec8c3aec82178c4840c14f
Alexandre Labranche <alexandrelabranche18@gmail.com>
2024-10-24 15:02:03 -0400

    New projects for lab 7

---
+++ Lab_7/src/A_Pattern.java
@@ -0,0 +1,16 @@
+public class A_Pattern {
+    public static void main(String[] args) {
+
+
+for(int i=1; i<= 6; i++) {
+
+
+for (int s=1; s<= i; s++){
+    System.out.print("*");
+}
+System.out.println();
+}
+
+
+    }
+}
---
+++ Lab_7/src/B_Pattern.java
@@ -0,0 +1,15 @@
+public class B_Pattern {
+    public static void main(String[] args) {
+
+        for(int i=6; i>= 1; i--) {
+
+
+            for (int s=1; s<= i; s++){
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
