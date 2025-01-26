f7e1bf0cc5dacff7f33b68a7ddf605568a666393
Alexandre Labranche <alexandre.labranche@hotmail.com>
2025-01-26 16:49:11 -0500

    Bmi calculator app for lab 1

---
+++ Lab 1/src/BmiCalculatorDemo.java
@@ -0,0 +1,52 @@
+// Import Scanner library for user input
+import java.util.Scanner;
+public class BmiCalculatorDemo {
+    public static void main(String[] args) {
+        // Create an instance of BmiCalculator class with values set to 0
+        BmiCalculator bmiCalc = new BmiCalculator(0, 0);
+        // Create scanner object for user input
+        Scanner scn = new Scanner(System.in);
+        // Variables to store mass and height inputs
+        double mass = 0;
+        double height = 0;
+        try {
+            // Prompt user to input mass
+            System.out.println("Enter Mass");
+            mass = scn.nextDouble();
+            // Validate that mass is within range (10 to 300)
+            if (mass >= 10 && mass <= 300) {
+                bmiCalc.setMass(mass);
+            } else {
+                System.out.println("Values below 10 and above 300 are not valid!");
+            }
+            // Prompt user to input height
+            System.out.println("Enter Height");
+            height = scn.nextDouble();
+            // Validate height is within range (0.2 and 2.2)
+            if (height >= 0.2 && height <= 2.2) {
+                bmiCalc.setHeight(height);
+            } else {
+                System.out.println("Values below 0.2 and above 2.2 are not valid!");
+            }
+            // Calculate BMI using BmiCalculator instance
+            double bmi = bmiCalc.calculateBmi();
+            // Determine BMI category based on calculated BMI value
+            String recommendationPhrase;
+            if (bmi <= 18.5) {
+                recommendationPhrase = "Underweight";
+            } else if (bmi > 18.5 && bmi <= 22.9) {
+                recommendationPhrase = "Normal";
+            } else if (bmi >= 23.0 && bmi <= 27.4) {
+                recommendationPhrase = "Moderately Overweight";
+            } else {
+                recommendationPhrase = "Obese";
+            }
+            // Output BMI value and its corresponding category
+            System.out.println("Based on your bmi of " + bmi + ", you are " + recommendationPhrase);
+
+        } catch(Exception e) {
+            // Handle non-numerical values
+            System.out.println("Value must be a number");
+        }
+    }
+}
