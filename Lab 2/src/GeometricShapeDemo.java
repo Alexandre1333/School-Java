5017609c5da2a2b7682055d5f5bd5c549a047f6f
Alexandre Labranche <alexandre.labranche@hotmail.com>
2025-02-12 10:54:46 -0500

    switched comments location

--- Lab 2/src/GeometricShapeDemo.java
+++ Lab 2/src/GeometricShapeDemo.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class GeometricShapeDemo {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        ArrayList<Circle> circles = new ArrayList<>(); // ArrayList for Circle object to store info
-        ArrayList<Rectangle> rectangles = new ArrayList<>(); // ArrayList for Rectangle object to store info
-        int userChoice; // initialize userChoice int for use in selecting menu option
-        try { // Try catch method to make sure application does not terminate without some type of error handling message
-            do { // Loops so that user can make as many objects as they want without the app immediately terminating
+        // ArrayList for Circle object to store info
+        ArrayList<Circle> circles = new ArrayList<>();
+        // ArrayList for Rectangle object to store info
+        ArrayList<Rectangle> rectangles = new ArrayList<>();
+        // initialize userChoice int for use in selecting menu option
+        int userChoice;
+        // Try catch method to make sure application does not terminate without some type of error handling message
+        try {
+            // Loops so that user can make as many objects as they want without the app immediately terminating
+            do {
                 // Menu options
                 System.out.println("1: Create a circle");
                 System.out.println("2: Create a rectangle");
@@ -22,7 +27,8 @@ public class GeometricShapeDemo {
                         String colour = scn.next();
                         System.out.println("Enter radius: ");
                         double radius = scn.nextDouble();
-                        circles.add(new Circle(colour, radius)); // adds the values entered by user into a new Circle array
+                        // adds the values entered by user into a new Circle array
+                        circles.add(new Circle(colour, radius));
                     }
                     case 2 -> {
                         // Creating a new rectangle
@@ -32,11 +38,13 @@ public class GeometricShapeDemo {
                         double height = scn.nextDouble();
                         System.out.println("Enter width: ");
                         double width = scn.nextDouble();
-                        rectangles.add(new Rectangle(colour, height, width)); // adds the values entered by user into a new Rectangle array
+                        // adds the values entered by user into a new Rectangle array
+                        rectangles.add(new Rectangle(colour, height, width));
                     }
                     case 3 -> {
                         System.out.println("Circles: ");
-                        for (Circle c : circles) System.out.println(c); // for-each loop, prints each object in Circle array
+                        // for-each loop, prints each object in Circle array
+                        for (Circle c : circles) System.out.println(c);
                         System.out.println("Rectangles: ");
                         for (Rectangle r : rectangles) System.out.println(r); // for-each loop, prints each object in Rectangle array
 
@@ -44,15 +52,17 @@ public class GeometricShapeDemo {
                     case 4 -> {
                         System.out.println("Exiting...");
                     }
+                    // Will show menu option again if user types incorrect menu option
                     default -> {
-                        System.out.println("Invalid choice. Please choose again."); // Will show menu option again if user types incorrect menu option
+                        System.out.println("Invalid choice. Please choose again.");
                     }
 
                 }
-            } while (userChoice != 4); // checks when 4 is entered as choice, if so, exit program
-
+                // checks when 4 is entered as choice, if so, exit program
+            } while (userChoice != 4);
+            // error checking for when user enters a non-numerical value or incorrect string
         } catch (Exception e) {
-            System.out.println("Please enter a correct numerical value or string"); // error checking for when user enters a non-numerical value or incorrect string
+            System.out.println("Please enter a correct numerical value or string");
         }
     }
 }
