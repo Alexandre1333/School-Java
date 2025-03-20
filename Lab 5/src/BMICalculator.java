f972e5af6b61413caa35a6eb2d240d3107d1d4c4
alex <alexandre.labranche@hotmail.com>
2025-03-20 12:11:52 -0400

    Start of assigment 5 based on the first one

---
+++ Lab 5/src/BMICalculator.java
@@ -0,0 +1,31 @@
+import java.lang.Math;
+// Default Constructor
+class BMICalculator {
+    private double mass;
+    private double height;
+
+    // Getter method for mass
+    public double getMass() {
+        return mass;
+    }
+    // Setter method for mass
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+    // Getter method for height
+    public double getHeight() {
+        return height;
+    }
+    // Setter method for height
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    // Method to calculate BMI. Dividing mass by height ^2
+    public double calculateBmi() {
+        return mass / Math.pow(height, 2);
+    }
+
+    public void returnSingaporeanBMIMeaning() {
+    }
+
+}
